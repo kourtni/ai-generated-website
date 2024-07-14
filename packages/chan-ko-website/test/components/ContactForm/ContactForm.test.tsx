600bb22e590b904bb598d1f3b1d77622960a173b
Kourtni Marshall <kourtni.marshall@gmail.com>
2024-07-14 03:24:36 -0500

    chore: Adds Vitest integration to chan-ko-website This adds proper configurations for using Vitest and it also includes a new test ContactForm.test.tsx which provides an example of the new test integration in action.

---
+++ packages/chan-ko-website/test/components/ContactForm/ContactForm.test.tsx
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ContactForm from '../../../src/components/ContactForm';
+
+describe('ContactForm', () => {
+  it('renders contact form', () => {
+    render(<ContactForm />);
+    
+    expect(screen.getByText('Contact Us')).toBeInTheDocument();
+    expect(screen.getByLabelText('First Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Company')).toBeInTheDocument();
+    expect(screen.getByLabelText('Work Email')).toBeInTheDocument();
+    expect(screen.getByLabelText('Job Title')).toBeInTheDocument();
+    expect(screen.getByLabelText('Country')).toBeInTheDocument();
+    expect(screen.getByLabelText('Reason For Contacting')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+  });
+});
